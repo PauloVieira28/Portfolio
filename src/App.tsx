f42493e3380c66d721821c1e0e920e0f7caf24fc
Paulo Vieira <paulo_vieira22@hotmail.com>
2025-08-09 14:34:23 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SideBar from './containers/SideBar'
+import AboutMe from './containers/AboutMe'
+import Projects from './containers/Projects'
+import GlobalStyle, { Container } from './styles'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <Container>
+        <SideBar />
+        <main>
+          <AboutMe />
+          <Projects />
+        </main>
+      </Container>
+    </>
+  )
 }
-
-export default App;
+export default App
---
+++ src/containers/SideBar/index.tsx
@@ -0,0 +1,12 @@
+import Title from '../../components/Title'
+
+const SideBar = () => {
+  return (
+    <aside>
+      <img src="https://github.com/PauloVieira28.png" />
+      <Title fontSize={20}>Paulo Cesar</Title>
+    </aside>
+  )
+}
+
+export default SideBar
